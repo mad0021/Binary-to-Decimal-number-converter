f5887d1b7d87be8728b17b1c71de2c74846b3146
mad0021 <123090580+mad0021@users.noreply.github.com>
2024-08-21 18:58:28 +0200

    Agregar archivos de proyecto.

---
+++ Binary-to-Decimal number converter/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Binary_to_Decimal_number_converter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, c, k;
+
+            Console.Write("Enter a 9 digit decimal number: ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nIn Binary: \n ");
+            for (c= 13; c >= 0; c--)
+            {
+                k = n >> c;
+                if (Convert.ToBoolean(k & 1))
+                    Console.Write("1");
+                else
+                    Console.Write("0");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Binary-to-Decimal number converter/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("Binary-to-Decimal number converter")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Binary-to-Decimal number converter")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("07e7e5d5-607b-4504-8315-15d3c7c47ccc")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
